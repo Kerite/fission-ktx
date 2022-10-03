e543aff832ce05966dda9dd69f389847c076faac
Kerite <keriteal985@gmail.com>
2022-10-03 14:41:55 +0800

    feat: 优化SimpleListRecyclerAdapter的用法

--- android-ktx/src/main/java/com/kerite/fission/android/ui/SimpleListRecyclerAdapter.kt
+++ android-ktx/src/main/java/com/kerite/fission/android/ui/SimpleListRecyclerAdapter.kt
@@ -8,48 +8,72 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.kerite.fission.AntiShaker
 import com.kerite.fission.android.ViewBindingInflateWithParent
 
-@Suppress("unused")
+/**
+ * A Simple List Recycler Adapter with item click event antiShaker
+ * 一个简单的 ListRecyclerAdapter 包括 ViewBinding 和 防止同时多个点击事件
+ * @param onBind Function Called in SimpleListItemViewHolder.bind()
+ * @param onItemClick Function called when item clicked
+ * @param antiShake Whether multiple click event triggered in the same time
+ * @author Kerite
+ */
+@Suppress("unused", "MemberVisibilityCanBePrivate")
 class SimpleListRecyclerAdapter<VB : ViewBinding, D : Any>(
     private val context: Context,
     private val itemBindingInflate: ViewBindingInflateWithParent<VB>,
-    private val onItemClick: (D) -> Unit,
-    private val onBind: VB.(D) -> Unit
-) : ListAdapter<D, SimpleListRecyclerAdapter.UniversalListItemViewHolder<VB, D>>(
-    object : DiffUtil.ItemCallback<D>() {
-        override fun areItemsTheSame(oldItem: D, newItem: D): Boolean = oldItem == newItem
+    private val onBind: VB.(D) -> Unit,
+    var onItemClick: (D) -> Unit = {},
+    var antiShake: Boolean = true
+) : ListAdapter<D, SimpleListRecyclerAdapter.SimpleListItemViewHolder<VB, D>>(object : DiffUtil.ItemCallback<D>() {
+    override fun areItemsTheSame(oldItem: D, newItem: D): Boolean = oldItem == newItem
 
-        @SuppressLint("DiffUtilEquals")
-        override fun areContentsTheSame(oldItem: D, newItem: D): Boolean = oldItem == newItem
+    @SuppressLint("DiffUtilEquals")
+    override fun areContentsTheSame(oldItem: D, newItem: D): Boolean = oldItem == newItem
+}) {
+    private val antiShaker = AntiShaker()
+    private val antiShakerProducer: () -> AntiShaker? = {
+        if (antiShake) this.antiShaker else null
     }
-) {
-    class UniversalListItemViewHolder<VB : ViewBinding, D>(
+    private val onItemClickProducer: () -> ((D) -> Unit) = { onItemClick }
+
+    /**
+     * View Holder of SimpleListRecyclerAdapter
+     * @author Kerite
+     */
+    class SimpleListItemViewHolder<VB : ViewBinding, D>(
         private val binding: VB,
-        private val onBind: VB.(D) -> Unit
+        private val onBind: VB.(D) -> Unit,
+        private val antiShakerProducer: () -> AntiShaker?,
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(data: D, onClick: (D) -> Unit) {
+        fun bind(data: D, onItemClickProducer: () -> ((D) -> Unit)) {
             binding.apply {
                 onBind(binding, data)
                 root.setOnClickListener {
-                    onClick(data)
+                    val antiShaker = antiShakerProducer()
+                    if (antiShaker == null) {
+                        onItemClickProducer()(data)
+                    } else {
+                        antiShaker.antiShake {
+                            onItemClickProducer()(data)
+                        }
+                    }
                 }
             }
         }
     }
 
     override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): UniversalListItemViewHolder<VB, D> {
-        return UniversalListItemViewHolder(
-            itemBindingInflate(LayoutInflater.from(context), parent, false),
-            onBind
+        parent: ViewGroup, viewType: Int
+    ): SimpleListItemViewHolder<VB, D> {
+        return SimpleListItemViewHolder(
+            itemBindingInflate(LayoutInflater.from(context), parent, false), onBind, antiShakerProducer
         )
     }
 
-    override fun onBindViewHolder(holder: UniversalListItemViewHolder<VB, D>, position: Int) {
+    override fun onBindViewHolder(holder: SimpleListItemViewHolder<VB, D>, position: Int) {
         val item = getItem(position)
-        holder.bind(item, onItemClick)
+        holder.bind(item, onItemClickProducer)
     }
 }
